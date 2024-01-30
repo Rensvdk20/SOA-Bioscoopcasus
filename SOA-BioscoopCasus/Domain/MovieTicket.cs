bfae0fe32ed13bc0e7108e8308f5a444579f8a03
Thijs Raaijmakers <TRaaijmakers32@gmail.com>
2024-01-30 12:20:01 +0100

    Add movieticket class

---
+++ SOA-BioscoopCasus/Domain/MovieTicket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class MovieTicket
+{
+	public MovieTicket()
+	{
+		private int rowNr;
+		private int seatNr;
+		private bool isPremium;
+		
+		public MovieTicket (int rowNr, int seatNr, bool isPremium)
+	{
+		this.rowNr = rowNr;
+		this.seatNr = seatNr;
+		this.isPremium = isPremium;
+	}
+
+
+	}
+}
