52fc0e0aaf70874e62b12a041fc6dd93032b0442
Rensvdk20 <rensvdk@gmail.com>
2024-01-30 12:19:08 +0100

    Added domain package

---
+++ SOA-BioscoopCasus/Domain/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOA_BioscoopCasus.Domain
+{
+    public class Order
+    {
+        private int OrderNr;
+        private Boolean isStudentOrder;
+    }
+}
