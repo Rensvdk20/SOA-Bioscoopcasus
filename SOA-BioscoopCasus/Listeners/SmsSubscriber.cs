2ca864cbd4bca0a1878fbaeae8edde1bfb1d3084
Rensvdk20 <rensvdk@gmail.com>
2024-02-21 14:13:49 +0100

    Added adapter

--- SOA-BioscoopCasus/Listeners/SmsSubscriber.cs
+++ SOA-BioscoopCasus/Listeners/SmsSubscriber.cs
@@ -1,17 +1,15 @@
 ﻿using SOA_BioscoopCasus.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using SOA_BioscoopCasus.ThirdPartyMessenger;
 
 namespace SOA_BioscoopCasus.Listeners
 {
     public class SmsSubscriber : ISubscriber
     {
+        private MessageAdapter _messageAdapter = new MessageAdapter();
+
         public void Update(string message)
         {
-            Console.WriteLine("SMS: " + message);
+            _messageAdapter.Send("SMS: " + message);
         }
     }
 }
---
+++ SOA-BioscoopCasus/ThirdPartyMessenger/MessengerProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOA_BioscoopCasus.ThirdPartyMessenger
+{
+    public class MessengerProvider
+    {
+        public void SendMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
